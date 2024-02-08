a27f92f230fcfc09095f961a2807ef9cf55638aa
Stepan <pakholkov@yandex.ru>
2024-02-09 01:52:06 +0300

    Исправил ошибки

--- UiKitDz/UiKitDz/CafeAliceViewController.swift
+++ UiKitDz/UiKitDz/CafeAliceViewController.swift
@@ -4,22 +4,22 @@
 import UIKit
 
 /// Класс где выставляется счет
-class CafeAliceViewController: UIViewController {
+final class CafeAliceViewController: UIViewController {
     // MARK: - IBOutlets
 
-    @IBOutlet var slayderOne: UISwitch!
-    @IBOutlet var slayderTwo: UISwitch!
-    @IBOutlet var slayderTheree: UISwitch!
+    @IBOutlet private var slayderOne: UISwitch!
+    @IBOutlet private var slayderTwo: UISwitch!
+    @IBOutlet private var slayderTheree: UISwitch!
 
     // MARK: - Public Methods
 
-    @IBAction func vipButton(_ sender: Any) {
+    @IBAction private func vipButton(_ sender: Any) {
         slayderOne.isOn = true
         slayderTwo.isOn = true
         slayderTheree.isOn = true
     }
 
-    @IBAction func billButton(_ sender: UIButton) {
+    @IBAction private func billButton(_ sender: UIButton) {
         let bilAlert = UIAlertController(title: "Выставить счет?", message: nil, preferredStyle: .alert)
         let action = UIAlertAction(title: "Чек", style: .default) { _ in
             self.performSegue(withIdentifier: "segue", sender: nil)
--- UiKitDz/UiKitDz/MainViewController.swift
+++ UiKitDz/UiKitDz/MainViewController.swift
@@ -3,25 +3,49 @@
 
 import UIKit
 
-/// class viewcontroller
-class MainViewController: UIViewController {
+/// Стартовый вью контроллер с проверкой логина и пароля
+final class MainViewController: UIViewController, UITextFieldDelegate {
     // MARK: - IBOutlets
 
-    @IBOutlet var passwordTextField: UITextField!
-    @IBOutlet var loginTextField: UITextField!
-    @IBOutlet var eyeButton: UIButton!
+    @IBOutlet private var passwordTextField: UITextField!
+    @IBOutlet private var loginTextField: UITextField!
+    @IBOutlet private var eyeButton: UIButton!
+    @IBOutlet var loginButton: UIButton!
 
     // MARK: - Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        passwordTextField.placeholder = "Введите пароль"
-        loginTextField.placeholder = "Введите почту"
+        setupUI()
     }
 
     // MARK: - Public Methods
 
-    @IBAction func textSecurity(_ sender: Any) {
+    func textField(
+        _ textField: UITextField,
+        shouldChangeCharactersIn range: NSRange,
+        replacementString string: String
+    ) -> Bool {
+        let loginText = loginTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        if loginText.count > 10, password.count > 5 {
+            loginButton.backgroundColor = .systemCyan
+            loginButton.layer.cornerRadius = 12
+            loginButton.isEnabled = true
+        }
+        return true
+    }
+
+    // MARK: - Private Properties
+
+    private func setupUI() {
+        passwordTextField.delegate = self
+        passwordTextField.placeholder = "Введите пароль"
+        loginTextField.placeholder = "Введите почту"
+        loginButton.isEnabled = false
+    }
+
+    @IBAction private func textSecurity(_ sender: Any) {
         if eyeButton.currentImage == UIImage(systemName: "eye.slash.fill") {
             eyeButton.setImage(UIImage(systemName: "eye"), for: .normal)
             passwordTextField.isSecureTextEntry = true
